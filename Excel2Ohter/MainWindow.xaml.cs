bfaae1393aae202068fa7603b62321ba1e2e898d
crazyoung <yangfeng666@sina.cn>
2019-06-25 15:21:17 +0800

    test e2j

--- Excel2Ohter/MainWindow.xaml.cs
+++ Excel2Ohter/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +24,30 @@ namespace Excel2Ohter
     {
         public MainWindow()
         {
-            InitializeComponent(); 
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Dictionary<string, object> dic = new Dictionary<string, object>();
+            List<string> list = new List<string>();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(i.ToString());
+            }
+            dic["1"] = list;
+            dic["2"] = "hhhhh";
+            dic["3"] = "xxxxx";
+            dic["4"] = "ccccc";
+
+            Dictionary<string, object> dic1 = new Dictionary<string, object>();
+            dic1["1"] = list;
+            dic1["2"] = "hhhhh";
+            dic1["3"] = "xxxxx";
+            dic1["4"] = "ccccc";
+
+            dic["5"] = dic1;
+            textBox1.Text = JsonConvert.SerializeObject(dic);
         }
     }
 }
